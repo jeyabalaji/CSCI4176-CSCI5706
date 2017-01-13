8559552f2bf935d70d2c98b914f42e81aecd5724
Jeya Balaji <jeyabalaji@JeyaBalajis-MacBook-Pro.local>
2017-01-13 06:50:49 -0400

    Lab 0 files

---
+++ lab0/app/src/main/java/cs/dal/lab0/MainActivity.java
@@ -0,0 +1,26 @@
+package cs.dal.lab0;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button b;
+    TextView v;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b= (Button) findViewById(R.id.button3);
+        v=(TextView)findViewById(R.id.textView);
+    }
+
+    public void click(View view){
+        v.setText("Im New");
+    }
+}
